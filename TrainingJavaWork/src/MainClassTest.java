8f69a9f9b54260814c30baa9e17f5e3bdd66d228
Alena Dobychina <Lesyonok.s@mail.ru>
2020-02-25 10:37:41 +0200

    Task 2

--- TrainingJavaWork/src/MainClassTest.java
+++ TrainingJavaWork/src/MainClassTest.java
@@ -12,4 +12,19 @@ public class MainClassTest extends MainClass {
             System.out.println("Ошибка! getLocalNumber должен возвращать 14, а возвращает " + a);
         }
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int check = this.getClassNumber();
+
+        if (check > 45)
+        {
+            System.out.println("Тест пройден. getClassNumber возвращает число > 45");
+        }
+        else {
+            System.out.println("Ошибка! getClassNumber должен возвращать число > 45, а возвращает " + check);
+        }
+    }
 }
+
